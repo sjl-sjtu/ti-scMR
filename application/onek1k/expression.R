b7ee710cbb927d3f25d4244582fb07f229e17c20
Sun Jianle <53631578+sjl-sjtu@users.noreply.github.com>
2025-04-13 16:03:02 -0400

    Update expression.R

--- application/onek1k/expression.R
+++ application/onek1k/expression.R
@@ -15,6 +15,45 @@ DimPlot(seurat_obj, reduction = "umap", pt.size = 1)
 seurat_obj <- FindVariableFeatures(seurat_obj, selection.method = "vst", nfeatures = 2000)
 seurat_obj <- ScaleData(seurat_obj)
 
+seurat_obj <- SetIdent(seurat_obj,value = "predicted.celltype.l2")
+
+#############
+# Harmony integration: remove batch effect
+library(harmony)
+seurat_obj <- RunHarmony(seurat_obj,"pool_number")
+seurat_obj <- seurat_obj %>% RunUMAP(reduction="harmony",dims=1:20) %>%
+  FindNeighbors(reduction="harmony",dims=1:20)%>%
+  FindClusters(resolution = 0.5)
+seurat_obj <- SetIdent(seurat_obj,value = "predicted.celltype.l2")
+DimPlot(seurat_obj, reduction = "umap", pt.size = 1)
+
+saveRDS(seurat_obj,"../onek1k_batchcorrect.rds")
+seurat_obj1 <- subset(seurat_obj, idents = c("B naive","B intermediate","B memory","Plasmablast"))
+DimPlot(seurat_obj1, reduction = "umap", pt.size = 1)
+
+
+########
+# trajectory inference
+library(SingleCellExperiment)
+library(slingshot)
+library(scater)
+library(scran)
+library(bluster)
+
+seurat_obj <- SetIdent(seurat_obj,value = "predicted.celltype.l2")
+DimPlot(seurat_obj, reduction = "umap", pt.size = 1)
+
+seurat_obj <- FindVariableFeatures(seurat_obj, selection.method = "vst", nfeatures = 2000)
+# Identify the 10 most highly variable genes
+# top10 <- head(VariableFeatures(seurat_obj), 10)
+# # plot variable features with and without labels
+# plot1 <- VariableFeaturePlot(seurat_obj)
+# plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
+# plot1+plot2
+# top2000 <- head(VariableFeatures(seurat_obj), 2000)
+# seurat_obj <- seurat_obj[top2000]
+seurat_obj <- ScaleData(seurat_obj)
+
 library(SingleCellExperiment)
 library(slingshot)
 library(scater)
@@ -28,6 +67,12 @@ seurat_obj2 <- subset(seurat_obj, idents = c("B naive","B intermediate","Plasmab
 
 sce <- as.SingleCellExperiment(seurat_obj2)
 
+# library(scPipe)
+# organism(sce) = "mmusculus_gene_ensembl"
+# gene_id_type(sce) = "ensembl_gene_id"
+# sce = calculate_QC_metrics(sce)
+# sce = detect_outlier(sce)
+# sce = remove_outliers(sce)
 plotUMAP(sce, colour_by="predicted.celltype.l2")
 # plotUMAP(sce, colour_by="donor_id")
 
@@ -44,6 +89,7 @@ p1 <- function(){par(mar = c(1,1,1,1));plot.new();
        col= sce$predicted.celltype.l2,#seurat_obj1$predicted.celltype.l2,
        pch = 16,cex=0.1);
   lines(SlingshotDataSet(sce)@curves$Lineage1, lwd=2, col='black');
+  # lines(SlingshotDataSet(sce)@curves$Lineage2, lwd=2, col='yellow')
 }
 p2 <- function(){par(mar = c(1,1,1,1));plot.new();
   legend("top", legend = unique(sce$predicted.celltype.l2), col = unique(sce$predicted.celltype.l2), 
@@ -68,6 +114,8 @@ df <- as.data.table(df,keep.rownames = T)
 df
 df %>% fwrite("plasa_batchcorr.csv")
 
+# library(phateR)
+# seurat_obj@reductions$phate <- phate(seurat_obj@assays$RNA@scale.data)
 seurat_obj2 <- subset(seurat_obj, idents = c("B naive","B intermediate","B memory"))
 sce <- as.SingleCellExperiment(seurat_obj2)
 plotUMAP(sce, colour_by="predicted.celltype.l2")
@@ -83,6 +131,7 @@ p1 <- function(){par(mar = c(1,1,1,1));plot.new();
        col= sce$predicted.celltype.l2,#seurat_obj1$predicted.celltype.l2,
        pch = 16,cex=0.1);
   lines(SlingshotDataSet(sce)@curves$Lineage1, lwd=2, col='black');
+  # lines(SlingshotDataSet(sce)@curves$Lineage2, lwd=2, col='yellow')
 }
 p2 <- function(){par(mar = c(1,1,1,1));plot.new();
   legend("top", legend = unique(sce$predicted.celltype.l2), col = unique(sce$predicted.celltype.l2), 
@@ -105,6 +154,7 @@ saveRDS(sce,"Bmemory_batchcorr.rds")
 df <- sce@colData
 df$slingshot <- NULL
 aggregate(slingPseudotime_1 ~ cell_type, data = df, FUN = mean)
+# aggregate(slingPseudotime_2 ~ cell_type, data = df, FUN = mean)
 df <- as.data.table(df,keep.rownames = T) 
 df %>% fwrite("B_memory_batchcorr.csv")
 
@@ -112,3 +162,48 @@ seurat_obj1 <- subset(seurat_obj, idents = c("B naive","B intermediate",'B memor
 saveRDS(seurat_obj1,"seurat_B_bc.rds")
 
 sce <- readRDS("plasa_batchcorr.rds")
+
+#####
+# DE genes
+seurat_obj <- seurat_obj[top2000]
+seurat_obj <- ScaleData(seurat_obj)
+seurat_obj <- RunPCA(seurat_obj, features = VariableFeatures(seurat_obj))
+ElbowPlot(seurat_obj)
+seurat_obj <- FindNeighbors(seurat_obj, dims = 1:15, reduction = "pca")
+seurat_obj <- FindClusters(seurat_obj, resolution = 0.5)
+
+seurat_obj <- RunUMAP(seurat_obj, dims = 1:15, n.components = 2)
+seurat_obj <- RunTSNE(seurat_obj, dims = 1:15, n.components = 2)
+seurat_obj <- SetIdent(seurat_obj,value = "cell_type")
+p1 <- DimPlot(seurat_obj, reduction = "umap", pt.size = 1)
+p2 <- DimPlot(seurat_obj, reduction = "tsne", pt.size = 1)
+p1+p2
+seurat_obj <- SetIdent(seurat_obj,value = "predicted.celltype.l2")
+p1 <- DimPlot(seurat_obj, reduction = "umap", pt.size = 1)+NoLegend()
+p2 <- DimPlot(seurat_obj, reduction = "tsne", pt.size = 1)
+p1+p2
+# cowplot::plot_grid(p1, p2, align="v", ncol=2,rel_heights = c(2, 1))
+
+markers <- FindMarkers(seurat_obj, ident.1 = "B memory", 
+                       ident.2 = "Plasmablast")
+markers %>% rownames_to_column(var = "gene") %>% as_tibble() %>% write_csv("B_markers.csv")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
